fe8be4644a613800b2773d32592a2fb9ac576a3a
kulitek <kulitekkardinah@gmail.com>
2026-03-11 17:15:57 +0700

    fix seeder

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -12,9 +12,11 @@ class UnitSeeder extends Seeder
      */
     public function run(): void
     {
-        Unit::create(['code' => 'U001', 'name' => 'Unit Farmasi']);
-        Unit::create(['code' => 'U002', 'name' => 'Unit Keuangan']);
-        Unit::create(['code' => 'U003', 'name' => 'Unit IT']);
-        Unit::create(['code' => 'U004', 'name' => 'Unit Rawat Inap']);
+        Unit::create(['code' => 'U001', 'name' => 'Unit Pelayanan']);
+        Unit::create(['code' => 'U002', 'name' => 'Unit Keperawatan']);
+        Unit::create(['code' => 'U003', 'name' => 'Unit Penunjang']);
+        Unit::create(['code' => 'U004', 'name' => 'Unit Perencanaan dan Pemasaran']);
+        Unit::create(['code' => 'U005', 'name' => 'Unit Umum']);
+        Unit::create(['code' => 'U006', 'name' => 'Unit Keuangan']);
     }
 }
